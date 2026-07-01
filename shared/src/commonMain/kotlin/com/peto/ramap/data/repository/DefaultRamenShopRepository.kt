285b7b4a2e1d6f057b0d5a6b109ce553a2acca23
chanho0908 <uni070@naver.com>
2026-07-01 10:18:05 +0900

    feat: 검색어 조회 data layer 구현

--- shared/src/commonMain/kotlin/com/peto/ramap/data/repository/DefaultRamenShopRepository.kt
+++ shared/src/commonMain/kotlin/com/peto/ramap/data/repository/DefaultRamenShopRepository.kt
@@ -3,6 +3,7 @@ package com.peto.ramap.data.repository
 import com.peto.ramap.data.datasource.shop.RamenShopDataSource
 import com.peto.ramap.domain.model.MapBounds
 import com.peto.ramap.domain.model.RamenShops
+import com.peto.ramap.domain.model.SearchQuery
 import com.peto.ramap.domain.repository.RamenShopRepository
 
 class DefaultRamenShopRepository(
@@ -13,6 +14,18 @@ class DefaultRamenShopRepository(
             value =
                 dataSource
                     .fetchRamenShops(bounds)
+                    .map { it.toDomain() }
+                    .associateBy { it.id },
+        )
+
+    override suspend fun searchRamenShops(
+        query: SearchQuery,
+        limit: Int,
+    ): RamenShops =
+        RamenShops(
+            value =
+                dataSource
+                    .searchRamenShops(query, limit)
                     .map { it.toDomain() }
                     .associateBy { it.id },
         )
